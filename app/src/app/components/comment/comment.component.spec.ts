3838d241fceb950f073b64f8706e5cda8df19812
Filipp Gusev <filipp.gusev@umbrellait.com>
2023-12-09 23:00:31 +0300

    add comment

--- app/src/app/components/comment/comment.component.spec.ts
+++ app/src/app/components/comment/comment.component.spec.ts
@@ -3,41 +3,64 @@ import { CommentService } from '../../services/comment.service';
 import { EMPTY, of } from 'rxjs';
 
 describe('CommentComponent', () => {
+
+  // 1. Импортируем наш тестируемый компонент CommentsComponent и сервис CommentsService.
   let component: CommentComponent;
   let service: CommentService;
 
+  // 2. Используем beforeEach для настройки начальных условий перед каждым тестом.
   beforeEach(() => {
-    const spyHttp = jasmine.createSpyObj('HttpClient', { post: of({}), get: of({}) })
+    // Создаем "шпиона" (спай) HttpClient с методами post и get, и передаем его в сервис.
+    const spyHttp = jasmine.createSpyObj('HttpClient', { post: of({}), get: of({}) });
     service = new CommentService(spyHttp);
+
+    // Создаем экземпляр тестируемого компонента и передаем ему созданный сервис.
     component = new CommentComponent(service);
   });
 
+  // 3. Тест на вызов метода getComments при инициализации компонента (ngOnInit).
   it('должен вызывать getComments при ngOnInit', () => {
+    // Создаем "шпиона" (спай) для метода getComments, который возвращает пустой observable.
     const spy = spyOn(service, 'getComments').and.callFake(() => {
       return EMPTY;
     });
 
+    // Вызываем ngOnInit компонента.
     component.ngOnInit();
 
+    // Проверяем, что метод getComments был вызван.
     expect(spy).toHaveBeenCalled();
   });
 
-  it('следует обновлять длину комментария после ngOnInit', () => {
-    const testComments = [1, 2, 3, 4]
-    spyOn(service, 'getComments').and.returnValue(of(testComments))
+  // 4. Тест на обновление длины комментариев после ngOnInit.
+  it('следует обновлять длину комментариев после ngOnInit', () => {
+    // Создаем тестовый массив комментариев.
+    const testComments = [1, 2, 3, 4];
 
+    // Заменяем реальный вызов getComments на возвращение тестового массива.
+    spyOn(service, 'getComments').and.returnValue(of(testComments));
+
+    // Вызываем ngOnInit компонента.
     component.ngOnInit();
 
+    // Проверяем, что длина массива комментариев в компоненте соответствует длине тестового массива.
     expect(component.comments.length).toBe(testComments.length);
   });
 
+  // 5. Тест на добавление нового комментария.
   it('добавить новый комментарий', () => {
-    const testComment = {text: 'test'}
-    const spy = spyOn(service, 'create').and.returnValue(of(testComment))
+    // Создаем тестовый комментарий.
+    const testComment = { text: 'test' };
 
-    component.add(testComment.text)
+    // Создаем "шпиона" (спай) для метода create, который возвращает тестовый комментарий.
+    const spy = spyOn(service, 'create').and.returnValue(of(testComment));
 
-    expect(spy).toHaveBeenCalled()
-    expect(component.comments.includes(testComment)).toBeTruthy()
+    // Вызываем метод add компонента, передавая текст тестового комментария.
+    component.add(testComment.text);
+
+    // Проверяем, что метод create был вызван и что тестовый комментарий был добавлен в массив комментариев в компоненте.
+    expect(spy).toHaveBeenCalled();
+    expect(component.comments.includes(testComment)).toBeTruthy();
   });
 });
+
--- app/src/app/components/test-co/test-co.component.spec.ts
+++ app/src/app/components/test-co/test-co.component.spec.ts
@@ -1,16 +1,21 @@
 import { TestCoComponent } from './test-co.component';
 
+// 1 экпортировали наш тестируемый компонент
 describe('TestCoComponent', () => {
+  // объявили переменную TestCoComponents
   let testCoComponent: TestCoComponent;
 
+  // 2 Через конструкцию beforeEach добавляем инициализацию компонента для каждого теста
   beforeEach(() => {
     testCoComponent = new TestCoComponent();
   });
 
+  // 3 Затем через конструкцию it добавлем тест нашего первого метода sayHello
   it('Передать привет Гарику', () => {
     expect(testCoComponent.sayHello('Гарик')).toBe('Привет Гарик');
   });
 
+  // 4 Теперь напишем второй тест для метода giveLangCode
   it('Должны быть коды стран: ru en ua', () => {
     const countries = testCoComponent.giveLangCode();
     expect(countries).toContain('ru');
@@ -18,6 +23,7 @@ describe('TestCoComponent', () => {
     expect(countries).toContain('ua');
   });
 
+  // 5 У нас остался не покрыт тестами, метод setColor.
   it('Цвет в eventEmitter должен быть: Black', () => {
     let result = '';
     testCoComponent.colorEmitter.subscribe(v => result = v);
